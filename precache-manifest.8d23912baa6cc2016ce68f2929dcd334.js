e9c38bf2aa3fdb777a04517a32045417ed94003a
whtalx <whtalx@gmail.com>
2019-05-19 12:33:41 +0300

    Updates

---
+++ precache-manifest.8d23912baa6cc2016ce68f2929dcd334.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3379b179fda394cf3c92e4dbb0076711",
+    "url": "/solitaire/index.html"
+  },
+  {
+    "revision": "5ecb7386c731541567b7",
+    "url": "/solitaire/static/css/main.ddc92b45.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/solitaire/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "5ecb7386c731541567b7",
+    "url": "/solitaire/static/js/main.31c0f4f4.chunk.js"
+  },
+  {
+    "revision": "3d4cc52b58bd19065a8a",
+    "url": "/solitaire/static/js/runtime~main.1eee08ec.js"
+  },
+  {
+    "revision": "1b683111cb3b0b8e497307c2c723c0d5",
+    "url": "/solitaire/static/media/fixedsys.1b683111.eot"
+  },
+  {
+    "revision": "2bc2af5ae204ff0563eafb651d1d9480",
+    "url": "/solitaire/static/media/fixedsys.2bc2af5a.woff2"
+  },
+  {
+    "revision": "2d6cb36925cca8267f0f8adaa59d60ed",
+    "url": "/solitaire/static/media/fixedsys.2d6cb369.woff"
+  },
+  {
+    "revision": "8ad84d20444bf9679cc5739424e9550f",
+    "url": "/solitaire/static/media/fixedsys.8ad84d20.ttf"
+  },
+  {
+    "revision": "f4a94cc308f2a598fe615a0d1e7d0b94",
+    "url": "/solitaire/static/media/fixedsys.f4a94cc3.otf"
+  },
+  {
+    "revision": "febd345c4e83620e6c94fb687878f8ce",
+    "url": "/solitaire/static/media/fixedsys.febd345c.svg"
+  },
+  {
+    "revision": "22d0534253eb7e27aba31c5715986c36",
+    "url": "/solitaire/static/media/tahoma.22d05342.woff"
+  },
+  {
+    "revision": "34736cf4a1bd22d9585b2c5f7d1f1c40",
+    "url": "/solitaire/static/media/tahoma.34736cf4.eot"
+  },
+  {
+    "revision": "35ffed31763ebda57fefb97598af6b86",
+    "url": "/solitaire/static/media/tahoma.35ffed31.ttf"
+  },
+  {
+    "revision": "3d49ec21e31e20bbacdb774a0e1982c2",
+    "url": "/solitaire/static/media/tahoma.3d49ec21.svg"
+  },
+  {
+    "revision": "468d605847fc188bd54d782c5a8a3952",
+    "url": "/solitaire/static/media/tahoma.468d6058.otf"
+  },
+  {
+    "revision": "dc2364073b49981ed6acacb3ee47702b",
+    "url": "/solitaire/static/media/tahoma.dc236407.woff2"
+  },
+  {
+    "revision": "7eaefa385ddcba4574ea603e204f051b",
+    "url": "/solitaire/static/media/wallpaper.7eaefa38.png"
+  }
+]);
