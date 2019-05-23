dadbe36acb65cdbd780466e7974184ad7df59615
whtalx <whtalx@gmail.com>
2019-05-23 18:30:41 +0300

    graphics, table layout

--- src/Sol/index.js
+++ src/Sol/index.js
@@ -15,19 +15,57 @@ export default function App() {
         <div className="menu-item">Help</div>
       </div>
       <div className="table">
-        <div className="deck card" />
-        <div className="waste card" />
-        <div className="foundation foundation-1st card" />
-        <div className="foundation foundation-2nd card" />
-        <div className="foundation foundation-3rd card" />
-        <div className="foundation foundation-4th card" />
-        <div className="tableau tableau-1st card" />
-        <div className="tableau tableau-2nd card" />
-        <div className="tableau tableau-3rd card" />
-        <div className="tableau tableau-4th card" />
-        <div className="tableau tableau-5th card" />
-        <div className="tableau tableau-6th card" />
-        <div className="tableau tableau-7th card" />
+        <div className="deck">
+          <div className="card ok" />
+        </div>
+        <div className="waste">
+          <div className="card notok" />
+        </div>
+        <div className="foundation foundation-A">
+          <div className="card opened ace hearts" />
+        </div>
+        <div className="foundation foundation-B">
+          <div className="card opened ace clubs" />
+        </div>
+        <div className="foundation foundation-C">
+          <div className="card opened ace diamonds" />
+        </div>
+        <div className="foundation foundation-D">
+          <div className="card opened ace spades" />
+        </div>
+        <div className="tableau tableau-A">
+          <div className="card opened 2 hearts" />
+        </div>
+        <div className="tableau tableau-B">
+          <div className="card closed back-astronaut">
+            <div className="card closed back-astronaut" />
+          </div>
+        </div>
+        <div className="tableau tableau-C" />
+        <div className="tableau tableau-D">
+          <div className="card closed back-astronaut">
+            <div className="card closed back-astronaut">
+              <div className="card closed back-astronaut">
+                <div className="card opened 10 diamonds">
+                  <div className="card opened jack spades" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="tableau tableau-E" />
+        <div className="tableau tableau-F" />
+        <div className="tableau tableau-G">
+        <div className="card opened 9 diamonds">
+            <div className="card opened 8 spades">
+              <div className="card opened 7 hearts">
+                <div className="card opened 6 clubs">
+                  <div className="card opened 5 hearts" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="status-bar">
         <div className="status-bar__text">Score: 1234567890!@#$%^&*()_+-=&nbsp;</div>
